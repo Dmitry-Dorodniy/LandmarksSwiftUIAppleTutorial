360823f5685ba84e5bbb788819d12e55f51e8d47
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-08-31 23:37:26 +0300

    Initial Commit

---
+++ LandmarksSwiftUIAppleTutorial/LandmarksSwiftUIAppleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSwiftUIAppleTutorialApp.swift
+//  LandmarksSwiftUIAppleTutorial
+//
+//  Created by Dmitry Dorodniy on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSwiftUIAppleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
