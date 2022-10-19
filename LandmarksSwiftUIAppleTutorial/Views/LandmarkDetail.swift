fa443d4b052915bb381b0c6f841512034c56e130
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-10-19 21:38:14 +0300

    add landmark detail

---
+++ LandmarksSwiftUIAppleTutorial/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  LandmarksSwiftUIAppleTutorial
+//
+//  Created by Dmitry Dorodniy on 15.10.2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/11/*@END_MENU_TOKEN@*/)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
