19fda4b119e0b4b13b2ac9c4b55d704e66e80f81
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-10-19 21:37:42 +0300

    add detail view with navigation link

---
+++ LandmarksSwiftUIAppleTutorial/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  LandmarksSwiftUIAppleTutorial
+//
+//  Created by Dmitry Dorodniy on 15.10.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+    }
+}
